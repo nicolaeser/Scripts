da179e648eaa471c4ad08edba6cc868f3f194944
Nico <67909777+gitipexa@users.noreply.github.com>
2022-06-05 19:02:46 +0200

    Add files via upload

---
+++ installer/dockercontainer/nextcloud.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+echo "############################################################################"
+echo "#                        Nextcloud Docker Installer                        #"
+echo "#                              by Nico L.                                  #"
+echo "#                   https://github.com/gitipexa/scripts                    #"
+echo "#                          Last Update: 05.06.2022                         #"
+echo "############################################################################"
+sleep 3
+clear 
+echo "############################################################################"
+echo "#                            Checking for Root                             #"
+echo "############################################################################"
+if (( $EUID != 0 )); then
+echo "############################################################################"
+echo "#                            Please run as root                            #"
+echo "############################################################################"
+    exit
+fi
+sleep 1
+clear
+echo "############################################################################"
+echo "#                             Checked for Root                             #"
+echo "############################################################################"
+sleep 2
+clear
+echo "############################################################################"
+echo "#                          Press Enter to continue                         #"
+echo "############################################################################"
+read continuepress
+sleep 1
+clear
+
+echo "############################################################################"
+echo "#                        Installing Nextcloud Docker                       #"
+echo "############################################################################"
+sleep 2
+echo "############################################################################"
+echo "#                       Server Update (apt update -y)                      #"
+echo "############################################################################"
+apt update -y
+wait -n
+echo "############################################################################"
+echo "#                       Server Upgrade (apt upgrade -y)                    #"
+echo "############################################################################"
+apt upgrade -y
+wait -n 
+echo "############################################################################"
+echo "#                           Enter the Name & Port                          #"
+echo "############################################################################"
+read -p "Please enter Nextcloud Container Name (nextcloud recommended): " nextcloudname
+read -p "Please enter Nextcloud Port 1 (8080 is recommended): " nextcloudport1
+read -p "Please enter Nextcloud Port 2 (80 is recommended): " nextcloudport2
+echo "############################################################################"
+echo "#            Downloading and Installing Nextcloud Container                #"
+echo "############################################################################"
+docker run -d -p $nextcloudport1:$nextcloudport2 --name $nextcloudname nextcloud 
+wait -n 
+clear
+echo "############################################################################"
+echo "#                     Installed Nextcloud Docker                           #"
+echo "#                                                                          #"
+echo "#      Nextcloud Name  | $nextcloudname                                    #"
+echo "#      Nextcloud Port1 | $nextcloudport1                                   #"
+echo "#      Nextcloud Port2 | $nextcloudport2                                   #"
+echo "#                                                                          #"
+echo "############################################################################"
+
---
+++ installer/dockercontainer/uptimekuma.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+echo "############################################################################"
+echo "#                        Uptimekuma Docker Installer                       #"
+echo "#                              by Nico L.                                  #"
+echo "#                   https://github.com/gitipexa/scripts                    #"
+echo "#                          Last Update: 05.06.2022                         #"
+echo "############################################################################"
+sleep 3
+clear 
+echo "############################################################################"
+echo "#                            Checking for Root                             #"
+echo "############################################################################"
+if (( $EUID != 0 )); then
+echo "############################################################################"
+echo "#                            Please run as root                            #"
+echo "############################################################################"
+    exit
+fi
+sleep 1
+clear
+echo "############################################################################"
+echo "#                             Checked for Root                             #"
+echo "############################################################################"
+sleep 2
+clear
+echo "############################################################################"
+echo "#                          Press Enter to continue                         #"
+echo "############################################################################"
+read continuepress
+sleep 1
+clear
+
+echo "############################################################################"
+echo "#                        Installing Uptimekuma Docker                      #"
+echo "############################################################################"
+sleep 2
+echo "############################################################################"
+echo "#                       Server Update (apt update -y)                      #"
+echo "############################################################################"
+apt update -y
+wait -n
+echo "############################################################################"
+echo "#                       Server Upgrade (apt upgrade -y)                    #"
+echo "############################################################################"
+apt upgrade -y
+wait -n 
+echo "############################################################################"
+echo "#                           Enter the Name & Port                          #"
+echo "############################################################################"
+read -p "Please enter Uptimekuma Container Name (uptimekuma is recommended)" uptimekumaname
+read -p "Please enter Uptimekuma Port 1 (3001 is recommended): " uptimekumaport1
+echo "############################################################################"
+echo "#            Downloading and Installing Uptimekuma Container               #"
+echo "############################################################################"
+docker run -d --restart=always -p $uptimekumaport1:$uptimekumaport1 -v uptime-kuma:/app/data --name $uptimekumaname louislam/uptime-kuma:1
+wait -n 
+clear
+echo "############################################################################"
+echo "#                     Installed Uptimekuma Docker                          #"
+echo "#                                                                          #"
+echo "#      Uptimekuma Name  | $uptimekumaname                                  #"
+echo "#      Uptimekuma Port1 | $uptimekumaport1                                 #"
+echo "#                                                                          #"
+echo "############################################################################"
+
